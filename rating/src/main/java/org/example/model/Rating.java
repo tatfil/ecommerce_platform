9c7f9e8f7cb6bcf66c1e307677fa78537a44dff6
t.filippova <t.filippova@andersenlab.com>
2023-05-19 13:35:17 +0300

    info for actuator

--- rating/src/main/java/org/example/model/Rating.java
+++ rating/src/main/java/org/example/model/Rating.java
@@ -10,8 +10,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
-import java.util.UUID;
 
 @Builder
 @NoArgsConstructor
